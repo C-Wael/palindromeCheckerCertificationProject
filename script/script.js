1d7c9c29b51bac15e4f03783987ea4b083039961
C Wael <wael.charni@outlook.fr>
2024-03-02 15:08:09 +0100

    Ajout de l'attribut onclick sur le check-btn, et ajout de la fonction fléchée emptyInput

--- script/script.js
+++ script/script.js
@@ -1,3 +1,9 @@
 const textInput = document.getElementById("text-input");
 const checkButton = document.getElementById("check-btn");
 const result = document.getElementById("result")
+
+const emptyInput = () => {
+    if (textInput.textContent === "") {
+        alert("Please input a value");
+    }
+};
