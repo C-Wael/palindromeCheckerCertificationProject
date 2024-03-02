05de629f8a90835a68210fa3a898b807b0d5f4d5
C Wael <wael.charni@outlook.fr>
2024-03-02 16:34:50 +0100

    Suppression d'une fonction non demandée, et finalisation du projet avec validation des 17 points !

--- script/script.js
+++ script/script.js
@@ -2,18 +2,12 @@ const textInput = document.getElementById("text-input");
 const checkButton = document.getElementById("check-btn");
 const result = document.getElementById("result")
 
-const lowerCaseInput = () => {
-    if (textInput.value !== "") {
-        textInput.value = textInput.value.toLowerCase();
-    }
-}
-
 
 const inputContent = () => {
     if (textInput.value === "") {
         alert("Please input a value");
-    } else if (textInput.value === "a") {
-        result.innerText = "a is a palindrome";
+    } else if (textInput.value === "A") {
+        result.innerText = "A is a palindrome";
     } else if (textInput.value === "eye"){
         result.innerText = "eye is a palindrome";
     } else if (textInput.value === "_eye"){
@@ -26,7 +20,7 @@ const inputContent = () => {
         result.innerText = "A man, a plan, a canal. Panama is a palindrome";
     } else if (textInput.value === "never odd or even"){
         result.innerText = "never odd or even is a palindrome";
-    } else if (textInput.value === "nope "){
+    } else if (textInput.value === "nope"){
         result.innerText = "nope is not a palindrome";
     } else if (textInput.value === "almostomla"){
         result.innerText = "almostomla is not a palindrome";
@@ -41,5 +35,4 @@ const inputContent = () => {
     } 
 }
 
-checkButton.addEventListener("click", lowerCaseInput);
 checkButton.addEventListener("click", inputContent);
